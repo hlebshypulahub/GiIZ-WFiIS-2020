fd89dc5d3d3b6078c588895ec3c4a1fc17443126
KRomanczyk <kas.romanczyk@gmail.com>
2021-05-04 19:25:47 +0200

    task5

---
+++ p2/build_random_k_reg_graph.m
@@ -0,0 +1,16 @@
+function g = build_random_k_reg_graph(n,k)
+
+a = repmat(k,1,n);
+if k>=n
+    error("Stopień wierzchołków musi być mniejszy!")
+end
+
+if is_degree_seq(a)
+    al = DS_to_AL(a)
+    am = AL_to_AM(al)
+    g=graph(am)
+    g = rand_graph(g,k)
+else
+    error("Nie można zbudować grafu k-regularnego dla danej liczby wierchołków")
+end
+
