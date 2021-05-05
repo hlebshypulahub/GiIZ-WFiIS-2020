228d79283b131e085633eeed0f233b2e6bbad7ef
KRomanczyk <kas.romanczyk@gmail.com>
2021-05-05 13:44:47 +0200

    fix task 6

--- p2/hamilton.m
+++ p2/hamilton.m
@@ -1,24 +1,31 @@
 function hamilton(am)
-
+%%postać wejściowa jako macierz sąsiedztwa
+%%reprezentacja g dla sprawdzenia spójności
 g = graph(am);
 c = components(g);
 [n,m] = size(am);
 
+%%jeśli graf spójny to wszystki wierzchołki należą do jednej składowej (nr
+%%1), więc tworzymy wektor jedynek dla sprawdzenia warunku
 o = repmat(1,n,1);
 
+%%przygotowanie stosu oraz tablicy do oznaczania wierzchołków odwiedzonych,
+%%oraz zmiennej dla wierzchołka poczatkowego
+stack = [];
+visited = repmat(0,1,n);
+vp = 1;        
+
+%%rozpoczynamy jeśli graf jest spójny
 if c==o
-    stack = [];
-    visited = repmat(0,1,n);
-    
     for i = 1:n
         if visited(i)==0
-             visited(i) = 1;
+             visited(i) = 1;        %%oznaczamy wierzchołek jako odwiedzony i wstawiamy na stos
              stack(end+1) = i;
-             hamilton_dfs_visit(am, n, i, i, stack,visited);
-             if size(stack)==n 
-                 %disp("Pelny")
-                 if am(v,stack(end))==1
-                   %disp("Sparwdza caly")
+             dfs_visit(i);       %%pierwsze wywołanie rekurencyjnej metody
+             [a,b] = size(stack);
+             if b==n
+                 if am(i,stack(end))==1     %%kontrolujemy czy wszystkie wierzchołki są na stosie 
+                   stack(end+1) = i;
                  end
              else
                 visited(i) = 0;
@@ -26,15 +33,37 @@ if c==o
              end
         end
     end
-    if size(stack)==0
-        error("Graf nie posiada cyklu hamiltona!")
+    [a,b] = size(stack);
+    if b==0
+        disp("Graf nie posiada cyklu hamiltona!")
     else
         disp("Cykl Hamiltona: ")
+        stack(end+1)=vp;
         disp(stack)
     end
 else
-    error("Graf musi być spójny!")
+    disp("Graf musi być spójny!")
+end
+
+%%przeszukujemy graf w głąb rekurencyjnie
+function dfs_visit(v)
+    for i = 1:n
+        if am(v, i)==1
+            if visited(i) == 0
+                visited(i) = 1;
+                stack(end+1) = i;
+                dfs_visit(i);
+                [a,b] = size(stack);
+                if b==n
+                    if am(vp,stack(end))==1
+                        return
+                    end
+                end
+                visited(i) = 0;
+                stack(end) = [];
+            end
+        end     
+    end
 end
 
 end
-
