25d70a26a96063488278f4e219bbab82bc1cdc4c
weronika3861 <weronikami@onet.pl>
2021-05-06 16:55:11 +0200

    add test eulerian cycle

---
+++ p2/test_eulerian_cycle.m
@@ -0,0 +1,21 @@
+%%% sprawdzenie czy funkcja get_eulerian_cycle jest poprawna
+function test_eulerian_cycle()
+    close all;
+    
+    %%% macierz która ma dwie spójne składowe połączone dwoma mostami
+    am=readmatrix('am.dat');
+    g=graph(am);
+        
+    draw_circle_graph(g);
+
+    cycle = get_eulerian_cycle(g);
+    [~, size_cycle] = size(cycle);
+
+    disp('Eulerian cycle:');
+    fprintf('[');
+    for i=1:size_cycle-1
+        fprintf('%d - ', cycle(i));
+    end
+    fprintf('%d]', cycle(size_cycle));
+    fprintf('\n');
+end
