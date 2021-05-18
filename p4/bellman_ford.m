a1a17a5745ee278380f917251ff6b7cf940b6ff6
weronika3861 <weronikami@onet.pl>
2021-05-18 23:02:56 +0200

    some changes in johnsons algorithm

--- p4/bellman_ford.m
+++ p4/bellman_ford.m
@@ -16,7 +16,7 @@ result = true;
 for k=1:n-1
     for i = 1:n
             for j = 1:n
-                if am(i,j)~=0 | (j==h | i==h)
+                if am(i,j)~=0 || (j==h || i==h)
                     if d(j) > d(i) + am(i, j)
                         d(j) = d(i) + am(i, j);
                         p(j) = i;
--- p4/johnson.m
+++ p4/johnson.m
@@ -14,7 +14,36 @@ function johnson(graph)
         %%% dzięki algorytmowi Bellmana-Forda uzyskujemy tablicę długości 
         %%% najkrótszych ścieżek od wierzchołka s: d
 
-        d
+%         d
+        
+        h=zeros(n,1);
+        for v=1:n
+            h(v) = d(v);
+        end
+%         h
+
+        [size_edges,~] = size(new_graph.Edges);
+
+        for i=1:size_edges
+%             new_graph.Edges.Weight(i) = 
+        end
+%         for edge in new_graph.get_edges():
+%             nodes = edge.get_nodes_ids()
+%             u = nodes[0].get_id()
+%             v = nodes[1].get_id()
+%             w = edge.get_weight()
+%             edge.set_weight(w + h[u] - h[v])
+% 
+%         cls.copy_weights(new_graph, graph)
+% 
+%         D = [[0 for x in range(len(graph.get_nodes()))] for y in range(len(graph.get_nodes()))]
+%         old_nodes = [i.get_id() for i in graph.get_nodes()]
+%         for u in old_nodes:
+%             for v in old_nodes:
+%                 dist, path = cls.dijkstra_directed(graph, u, v)
+%                 D[u][v] = dist - h[u] + h[v]
+%      
+
 
 end
 
