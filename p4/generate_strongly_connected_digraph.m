8d11e18a55d651fcbca333ed91860f2022763f23
KRomanczyk <kas.romanczyk@gmail.com>
2021-05-18 21:13:08 +0200

    commit

--- p4/generate_strongly_connected_digraph.m
+++ p4/generate_strongly_connected_digraph.m
@@ -2,15 +2,7 @@ function g = generate_strongly_connected_digraph(n,p)
 
 strong_comp = repmat(1,1,n);
 components =repmat(0,1,n);
-i=0;
-%{
-while ~isequal(components,strong_comp)
-    g = build_random_digraph(n, p);
-    am = full(adjacency(g));
-    components = kosaraju(am);
-    i=i+1;
-end
-%}
+
 for i = 0:100
     g = build_random_digraph(n, p);
     g = set_weights(g, -5, 10);
@@ -20,14 +12,7 @@ for i = 0:100
         break     
     end
 end
-i
-%am = full(adjacency(g));
-%comp = kosaraju(am)
-%components
-%strong_comp
 
 fig = draw_directed_weighted_graph(g);
-        
-%disp('Nie udało się zbudować grafu')
 
 end
