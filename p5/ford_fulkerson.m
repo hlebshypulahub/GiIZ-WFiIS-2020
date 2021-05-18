9e8f9f141e61ae08e07112bcc7c8010b4476d73f
Katarzyna Romańczyk <62744728+kasiaromanczyk@users.noreply.github.com>
2021-05-19 01:30:59 +0200

    Update ford_fulkerson.m

--- p5/ford_fulkerson.m
+++ p5/ford_fulkerson.m
@@ -2,13 +2,35 @@ function ford_fulkerson(g, layer_nodes)
 
 %%wagi na pewno weźmiemy z macierzy sąsiedztwa:
 am = full(adjacency(g, 'weighted'));
-[n,m]=size(am)
+[n,m]=size(am);
 %%wyzerowanie przepływów (??)
-f = zeros(n)
+f = zeros(n);
+
+
+%%tworzymy sieć rezydualną:
+res_am = zeros(n)
+
+for i = 1:n
+    for j = 1:n
+        if am(i,j)~=0
+            res_am(i,j) = am(i,j); %-f(i,j)     %to przypisanie nie działa niestety
+        elseif am(j,i)~=0
+            res_am = f(i,j);  
+        else
+            res_am(i,j)=0;    
+        end
+    end
+end
+res_am
+res_g = digraph(res_am);
+figure
+%%tworzy ok raczej, można by wyrysować tak jak sieć ale nie wiem jak z
+%%layer nodes
+fig = draw_directed_weighted_graph(res_g);
 
 %%w layer nodes mamy ilość wierzchołków w każdej warstwie,
 layer_nodes
-[n, m] = size(layer_nodes)
+[n, m] = size(layer_nodes);
 sum=1;
 for i = 2:n
     for j = layer_nodes{i}
@@ -16,15 +38,16 @@ for i = 2:n
         sum=sum+1;
     end
 end
-sum %sprawdza czy dobrze zliczyło
+sum; %sprawdza czy dobrze zliczyło ilość wierzchołków od źródła do ujścia
 
-while bfs(g,1,sum)
+%bfs(res_g,1,sum);
+%while bfs(res_g,1,sum) %tu jakoś warunek na przesukiwanie wszerz w sieci rezudualnej% 
+    %%bfs zwraca ścieżkę
+    %path = bfs(res_g,1,sum)
+    
+    %tu warunki 
     
     
-end
-
-
-
-
+%end
 
 end
