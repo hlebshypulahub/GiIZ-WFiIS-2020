09d6a87b0afcdc6a13bdc4cc0706102f40192571
Katarzyna Romańczyk <62744728+kasiaromanczyk@users.noreply.github.com>
2021-05-19 01:29:24 +0200

    Update bfs.m

--- p5/bfs.m
+++ p5/bfs.m
@@ -1,18 +1,37 @@
 function result = bfs(g,s,t)
-
+%%bfs działa ok dla prawidłowego grafu
 d = Inf(t, 1);
 d(s) = 0;
 p = null(t, 1);
 
+am = full(adjacency(g, 'weighted'));
+
 Q = []; %kolejka Q
-%Q(end+1) = s; %wstawiam s na początek
+Q(end+1) = s; %wstawiam s na początek
 %komentarz bo pętla na razie nie dokończona, żeby wrakunek był prawdziwy
 
-while ~isequal(Q,[])
-    
-   disp('do') 
+while ~isequal(size(Q,2),0)
+    v = Q(end);
+    Q(end) = [];
+    for j = 1:t
+        if am(v,j)~=0
+            if d(j)== Inf
+                d(j) = d(v) +1;
+                p(j)=v;
+                Q(end+1)=j;
+                if j==t  %%warunek przerwania, jeżeli mamy już scieżkę do źródła
+                    break
+                end
+            end
+        end
+    end
 end
 
-%result false aby pętla w wywołaniu na razie nie chodziła w nieskończoność
-result = false
+
+result = []
+while ~(p(t)==s)
+    result(end+1) = p(t)
+    t=p(t)
+end
+result = flip(result)
 end
